4c65ecb75397074bd2656216eb6ead8c43c596ae
heather <hjohnsick@yahoo.com>
2020-01-29 08:41:52 -0500

    Get to know your classmates lab 2nd attempt

--- Lab8GetToKnowYourClassmates/Program.cs
+++ Lab8GetToKnowYourClassmates/Program.cs
@@ -6,10 +6,13 @@ namespace Lab8GetToKnowYourClassmates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to our C# class Which student would you like to learn more about? (enter a number 1 - 5): ");
-            int student = int.Parse(Console.ReadLine());
-            //I don't  know  how to pull out the information to use it from the array
+            // 1. user is going to enter 1-5 .. link to student ..
+            // 2. incorrect prompt
+            // 3. adrres and favo food..
 
+            Console.WriteLine("Welcome to our C# class Which student would you like to learn more about? (enter a number 1 - 5): ");
+            int studentNumber = int.Parse(Console.ReadLine());
+            //I don't  know  how to pull out the information to use it from the array
             var namesList = new string[5, 3]
            {
 
@@ -19,12 +22,18 @@ namespace Lab8GetToKnowYourClassmates
                 { "Dawn", "Marysville", "tacos"},
                 { "Sandy", "Smiths Creek", "pancakes"},
            };
-            
-            for (var i = 0; i < namesList.Length; i++)
+            if (namesList.Length < studentNumber)
             {
-                Console.WriteLine(namesList[i]);
+                Console.WriteLine("Invalid Value");
+                return;
             }
 
+            
+
+            Console.WriteLine($"Student is {namesList[studentNumber, 0]}, Hometown is {namesList[studentNumber, 1]}, Favorite food is {namesList[studentNumber, 2]}");
+            Console.ReadLine();
+
+         
 
         }
        
